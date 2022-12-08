c85b0842c83d0a7d7c0d8c60d21228521151027c
He Xiang Koh <kohhxx6@gmail.com>
2022-12-08 15:43:31 +0800

    Change normal function to arrow function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-function App() {
-  return <div>Hello World!</div>;
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>Hello World</div>
+  )
 }
 
-export default App;
+export default App
